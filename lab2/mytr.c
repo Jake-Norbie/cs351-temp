b7e66c58cf6d30f784afc53d3741976385ae6540
Jake-Norbie <jnorbie@hawk.iit.edu>
2022-02-07 18:49:51 -0600

    Finished normal tr usage

--- lab2/mytr.c
+++ lab2/mytr.c
@@ -3,11 +3,24 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  if (argc < 3) {
+  if (argc != 3) {
     printf("Usage: %s STRING1 STRING2\n", argv[0]);
     printf("       %s -d STRING\n", argv[0]);
     exit(1);
   }
-  
+  char str[100];
+  fgets(str, 100, stdin);
+  for (int l = 0; l < strlen(str); l++) {
+     char *pos;
+     pos = strchr(argv[1],str[l]);
+     if (pos) {
+       printf("%c",argv[2][pos - &argv[1][0]]);
+     }
+     else {
+       printf("%c",str[l]);
+     }
+  }
+  printf("%d",&argv[1][1]); 
+  exit(0); 
   return 0;
 }
